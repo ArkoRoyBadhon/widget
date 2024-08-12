e7d1d8439120bb9237ce89293b8a8deecf5e8c1e
ArkoRoyBadhon <67919432+ArkoRoyBadhon@users.noreply.github.com>
2024-08-12 12:59:12 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,168 +1,4 @@
-Arko Roy Badhon
-arkoroy12
-Online
 
-najmul islam sakib — 08/06/2024 1:55 PM
-Inshallah🔥
-Arko Roy Badhon — 08/06/2024 2:14 PM
-Sonibar??
-kazirayhan — 08/06/2024 2:15 PM
-mostly
-kazirayhan — 08/07/2024 7:35 PM
-https://chromewebstore.google.com/detail/testimonial/gojdbimennlpdoagkgongbdecafpccim?hl=en-US&utm_source=ext_sidebar
-Testimonial - Chrome Web Store
-Get testimonials from your customers with ease.
-Image
-https://testimonial.to/
-Testimonial | The easiest way to collect and display testimonials
-With Testimonial, in minutes, you can collect and display video testimonials and other types of testimonials from your customers with no need for a developer or website hosting.
-Testimonial | The easiest way to collect and display testimonials
-@everyone  sobai ektu ei system ta reseach koren. funcanality ja ja ace. priority set koren. Konta easy mid hard eivabe
-Jno, sobar modde valo ekta outline thake ei system ta niye
-!important
-kazirayhan — 08/08/2024 12:24 PM
-@everyone  any update
-RiaJul Pro — 08/08/2024 8:25 PM
-ami dekhichilam unader application ta, kintu kivabe level take sajabo seta bujhte parchina. any suggestion thakle janate paren.
-kazirayhan — 08/08/2024 9:06 PM
-ei msg ta bujen
-RiaJul Pro — 08/08/2024 9:07 PM
-bhai easy middle and hard bolte kontake bujhiyechen?
-kiser vittite ei level ta korbo?
-kazirayhan — 08/09/2024 10:28 PM
-@Arko Roy Badhon  @najmul islam sakib  apnader update ace kono
-kazirayhan — 08/09/2024 10:32 PM
-testimonial, website ta visit koren, then okahanar sob funcanality ghula dekhen. tarpor as a developer hisabe kon kon fn ghula korte easy mid ba hard hobe. eitar list korte bolcilam. aponi msg ta valo kore poren nai. baki ra jodi buje to apnr jonno ekta bebosta korbo. first day te. jkhon kaj suru hbe
-RiaJul Pro — 08/09/2024 10:34 PM
-Bhai proti hingsa theke sobai beche thakar cesta kori. ekhane kono kichu na bujhle jigges korata kono onnay kaj na. ar apni bolar pore ami oita dekhechi, amar kache kothin bolte mone hoyeche zeta seta holo kivabe live embaded system ta korte hobe. zehetu unara iframe diye kajta koreche.
-kazirayhan — 08/09/2024 10:34 PM
-ei problem ghula list koren
-Mone koren, ekta math e sob line e kintu problem thake na. 1,2 line question thekei likte hoi, to aponara amak outline tai dekhan nai
-aponi inbox e msg den, eita discussion er jonno. personal msg diben na
-STOP! writting personal msg here
-RiaJul Pro — 08/09/2024 10:37 PM
-eita apni bhai thik kotha bolchen ekhane dosta amderoi. ami na jananor jonno sorry. karon onnoder sathe zogazog korteo parini ar beparta clear o hote parini.
-kazirayhan — 08/09/2024 10:38 PM
-eita ekhan theke remove koren, aponr personal kono query hole, amak msg koren
-kazirayhan — 08/09/2024 10:40 PM
-hmm, na buje kotha bolte hoi na. age khujben then bolben. tahole aponader k kew hold korate parbe na. age vabben j aponi j bolcen ta verifiyed ki na. apnader ei problem ta ekhono ace. r ami ekhane personally kotha boli na. aponader k ja bolci hoi inbox or meet a. 
-Again @everyone  ei outline ta important. eita clear koren qucikly. eita clear na hole amra kaj start korte parbo na.
-RiaJul Pro — 08/09/2024 10:52 PM
-https://docs.google.com/document/d/1e37m5f76sAioInNU0ZFh2hSuSRLNPtY3nln1WWT69aU/edit?usp=sharing
-Google Docs
-About Testimonial
-About Testimonial.toEasy steps:1. User can make testimonial spaces for their website 2. They can easily add video and text testimonial 3. All testimonial data will be stored under the space into the database 4. Owner can see the added testimonials Mid-level steps: Arranging data table Adding ...
-Image
-kazirayhan — 08/09/2024 10:55 PM
-eita te onek feature ace, @everyone  sobai eita add koren. then amak complete kore janan. then ami ekta dibo oitar shate eita match koraben. aponara 3jon e eighula r ami 1 jon e korcilam. So, @everyone  sobai contribute koren
-Arko Roy Badhon — 08/10/2024 11:12 AM
-@kazirayhan vai
-https://docs.google.com/document/d/1Rr41WWSHQQ72HmTY0Y_fcI7oQNsKJtaYnRvNEzk06WE/edit?usp=sharing
-
-amr ta
-Google Docs
-Testimonial Site
-Testimonial Site Sign Up ⇒ custom sign up does not work. Only google oauth working Dashboard ⇒ have a section for overview ⇒ have a section for work space ⇒ have 2 option to start a space - create from scratch & with ai assistance =====> several options for creating a space ⇒ basic ⇒ thank yo...
-Image
-kazirayhan — 08/10/2024 12:28 PM
-@najmul islam sakib
-apnr ta koi
-najmul islam sakib — 08/10/2024 12:43 PM
-https://docs.google.com/document/d/1p6oVZI4ScMO2yOKqseih0k3DGz2hLBnzDvbRKgagbZg/edit?usp=sharing
-Google Docs
-Testimonial.io
-Easy:i. Testimonial spaces (Create, delete, etc…) ii. Text or video upload as testimonials iii. The site owner can see testimonials iv. Affiliate link v. Custom button  Medium: i. Subscription planning ii. Custom video duration iii. notificationHard:i. Social media reviews ii. Embedded sc...
-Image
-kazirayhan — 08/10/2024 1:01 PM
-https://docs.google.com/document/d/13-kpLhg8VSD2EJknchcE2VXoljzVOLjZoLkjZoLh4ic/edit?usp=sharing
-Google Docs
-Testimonial.to for the dev
-Account Creation and User Authentication Landing page User registration and login Email verification Password reset functionality Billing Plan Account security, 2FA with phone number Account Change Profile pic and name User Dashboard and Space Management User dashboard to create and ma...
-Image
-@everyone  eita dekhen
-jmon organize ace, oivabe rakhen, then sort kore felen ekhn, priority hisabe
-then ekta final outile kore amak den
-Arko Roy Badhon — 08/10/2024 3:22 PM
-https://docs.google.com/document/d/17cCAH1_rv6C3xbnq5Ul8z8W5k0jkrUp3gLk9LLoA_kU/edit?usp=sharing
-
-updated document
-Google Docs
-Overview of Testimonial
-Final overview and listing for the project Testimonial.to Contributor: Arko, Sakib & RiaJul Easy Steps: Account Creation and User Authentication Landing page User registration and login Email verification Password reset functionality Billing Plan Account security, 2FA with phone number Ac...
-Image
-kazirayhan — 08/10/2024 6:43 PM
-@everyone  eita ektu review koren, amr list theke match koren. kicu bad porce. aponara 3 jon e korcen to missing hobar kotha cilo na. jaihok dekhen
-kazirayhan — Yesterday at 11:40 AM
--Users can import testimonials from these social media (see video: https://www.youtube.com/watch?v=Dn1Zs2EHYg4   https://www.youtube.com/watch?v=Oqok1A8HHwc ) 
--Users after leaving video or text testimonial can share on social media: Twitter, Instagram, Facebook, Tik Tok, Linkedin
-YouTube
-Testimonial
-How to Embed Google Reviews On Any Website
-Image
-YouTube
-Testimonial
-How to import 3rd-party reviews on Testimonial.to
-Image
-Edit Testimonial (choose video thmbnail, cut videol)
--Trial period set
-Eighula ektu add kore den
-@everyone
-kazirayhan — Yesterday at 2:27 PM
-@everyone  is it done?
-Arko Roy Badhon — Yesterday at 2:29 PM
-2 ta point add korci.
-r vai ai site ta to banate hard hbe onek
-kazirayhan — Yesterday at 2:41 PM
-kon kon topic hard lagbe, amak list kore janan
-Arko Roy Badhon — Yesterday at 3:44 PM
-vai amder doc er moddei hard part gula bole dewa hoice. erpor kaj er somoy aro onek complication aste pare,
-kazirayhan — Yesterday at 3:44 PM
-new point ghula add korcen
-Arko Roy Badhon — Yesterday at 3:46 PM
-==> 3rd Party Integrations
-Integrations of social media
-Import testimonials from social medias
-Integrations of external videos
-Other feature and integrations
-==> Edit Testimonial
-Edit Testimonial (choose video thumbnail, cut video)
-Trial period set
-==> Custom Domain
-
-
-agula add korci
-kazirayhan — Yesterday at 3:49 PM
-Okay, amra document ta niye agamikal bosbo. sobai right time e cole asben
-working hour
-kazirayhan — Today at 10:08 AM
-@najmul islam sakib  bhai, acen
-bakira ektu wait koren, sakib bhai er jonno
-najmul islam sakib — Today at 12:28 PM
-Ji vai achi
-kazirayhan — Today at 12:31 PM
-baki ra ki acen sobai
-Arko Roy Badhon — Today at 12:31 PM
-aci
-kazirayhan — Today at 12:31 PM
-@RiaJul Pro
-Arko Roy Badhon — Today at 12:32 PM
-riajul vai chilo, akni asbe
-https://docs.google.com/document/d/17cCAH1_rv6C3xbnq5Ul8z8W5k0jkrUp3gLk9LLoA_kU/edit?usp=sharing
-Google Docs
-Overview of Testimonial
-Final overview and listing for the project Testimonial.to Contributor: Arko, Sakib & RiaJul Easy Steps: Account Creation and User Authentication Landing page User registration and login Email verification Password reset functionality Billing Plan Account security, 2FA with phone number Ac...
-Image
-kazirayhan — Today at 12:53 PM
-// widget.js
-(function() {
-    // Create the button
-    var button = document.createElement('button');
-    button.id = 'data-fetch-button';
-    button.innerText = 'Show Posts';
-Expand
-message.txt
-3 KB
-﻿
 // widget.js
 (function() {
     // Create the button
@@ -238,5 +74,4 @@ message.txt
     document.body.appendChild(button);
     document.body.appendChild(card);
 })();
-message.txt
-3 KB
+
